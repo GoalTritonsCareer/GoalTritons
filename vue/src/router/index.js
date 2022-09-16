d04ca701c80e5523e374f90c944e0d68dc7deda9
wenwill <j6wen@ucsd.edu>
2022-09-15 17:44:13 -0700

    merge request2

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -10,7 +10,6 @@ const routes = [
     children: [
       { path: 'home', name: 'Home', component: () => import("../views/Home.vue") },
       { path: 'resume', name: 'Resume', component: () => import("../views/Resume.vue") },
-      { path: 'networking', name: 'Networking', component: () => import("../views/Networking.vue") },
       { path: 'fakeNetworking', name: 'FakeNetworking', component: () => import("../views/FakeNetworking.vue") },
       { path: 'interview', name: 'Interview', component: () => import("../views/Interview.vue") }
     ]
