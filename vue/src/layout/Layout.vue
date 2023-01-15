b9afe2810ba9d2b920a8223719149149bed39480
HuanerW <97192419+HuanerW@users.noreply.github.com>
2023-01-15 14:20:05 -0800

    Add files via upload

--- vue/src/layout/Layout.vue
+++ vue/src/layout/Layout.vue
@@ -36,6 +36,54 @@ export default {
 </script>
 
 <style scoped>
+/* 大屏幕 ( > 13 in. ) */
+@media (min-width: 1440px) {
+  .header {
+    width: 1440px;
+  }
+  .common-layout {
+    width: 1440px;
+    margin-left: calc((100vw - 1440px)/2);
+  }
+}
+/* 多数电脑 ( 11 in. ≤ size ≤ 13 in. ) */
+@media (min-width: 1280px) and (max-width: 1439px) {
+  .header {
+    width: 1280px;
+  /*  margin-left: calc((100vw - 1280px)/2);*/
+  }
+  .wrapper__body{
+    margin-left: calc(1vw + 350px) !important;
+  }
+  .wrapper__footer{
+    margin-left: calc(1vw + 350px) !important;
+  }
+}
+/* 小电脑及大Pad */
+@media (min-width: 992px) and (max-width: 1279px) {
+  .header {
+    width: 992px;
+    margin-left: calc((100vw - 992px)/2);
+  }
+}
+/* 多数Pad */
+@media (min-width: 768px) and (max-width: 991px) {
+  .header {
+    width: 768px;
+    margin-left: calc((100vw - 768px)/2);
+  }
+}
+/* 手机 */
+@media (min-width: 480px) and (max-width: 767px) {
+  .header {
+    width: 480px;
+    margin-left: calc((100vw - 767px)/2);
+  }
+}
+.header {
+  position: fixed;
+  z-index: 20;
+}
 .wrapper {
   position: relative;
 }
